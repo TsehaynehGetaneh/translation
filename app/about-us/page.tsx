7ceb7b5c90ae65fe084337b18057c076a45a46b2
ead8 <mststy8@gmail.com>
2024-03-28 23:46:02 +0300

    about-us update

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -10,39 +10,25 @@ import Image from 'next/image'
 
 const Page: React.FC = () => {
   return (
-    <div>
+    <div >
       <Image src={village} height={200} alt="Banner Image" className=" w-full mx-auto rounded-lg  md:h-full object-cover filter " />
-    <div className="flex flex-col items-center">
-      <h1 className="text-4xl font-bold my-8 mx-20 text-center text-blue-400">
+    <div className="flex flex-col items-center mx-20 ">
+      <h1 className="text-4xl font-bold my-8 max-w-4xl text-center text-blue-400">
         We are a leading translation company providing language services for
         clients worldwide
       </h1>
-      <div className="flex flex-col md:flex-row mt-8">
-        <div className="md:w-1/2">
-          <img
-            src={aboutUsData.aboutUsIntro.image}
-            alt="About Us Intro Image"
-            className="w-full"
-          />
-        </div>
-        <div className="md:w-1/2 mt-4 md:mt-0">
-          <p>{aboutUsData.aboutUsIntro.text}</p>
-        </div>
-      </div>
-      <div className="w-full mx-auto flex lg:justify-between justify-center items-center flex-col lg:flex-row">
-  <div className="lg:w-[50%] w-[90%]">
-
-
-</div>
-
-  </div>
-  <div className="relative lg:ml-0 ml-36 mt-5" >
-  <Image src={dots} width={400}  alt='dots' className='absolute top-10 -left-20 -z-10 rounded w-20' />
-  <Image src={translationRelative} width={400} height={350} alt='transltion-relative' className='rounded-md ' />
-  <Image src={tranlationPerson} width={280} alt='transltion-relative' className='absolute -left-36 bottom-0 rounded-md' />
+      <div className="flex flex-col lg:flex-row mt-8 lg:gap-x-48 justify-center items-center mx-auto max-w-screen-xl">
+      <div className="lg:w-1/2 mt-4 md:mt-0 leading-relaxed p-2">
+        <p className="text-sm tracking-wide leading-6">{aboutUsData.aboutUsIntro.text}</p>
+    </div>
+    <div className="relative lg:ml-0 ml-36 mt-5">
+        <Image src={dots} width={400} alt='dots' className='absolute top-10 -left-20 -z-10 rounded w-20' />
+        <Image src={translationRelative} width={400} height={350} alt='transltion-relative' className='rounded-md ' />
+        <Image src={tranlationPerson} width={280} alt='transltion-relative' className='absolute -left-36 bottom-0 rounded-md' />
+    </div>
 </div>
 </div>
-      <div className="grid grid-cols-2 w-full p-10 rounded-md md:grid-cols-4 gap-4 items-center"
+      <div className="grid grid-cols-2 mt-10 w-full p-10 rounded-md md:grid-cols-4 gap-4 items-center"
       style={{ 
         backgroundImage: `url(${typeof statimage === 'string' ? statimage : statimage.src})`, 
         backgroundSize: 'cover', 
@@ -55,48 +41,54 @@ const Page: React.FC = () => {
       <StatisticsCard number={400} label="Professional Translators" />
     </div>
 
-      <div className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-14">
-        <div className="md:w-1/2 pt-28">
-          <img
-            src={aboutUsData.ourStory.image}
-            alt="Our Story Image"
-            className="w-full"
-          />
-        </div>
-        <div className="md:w-1/2 mt-4 md:mt-0">
-            <h2 className="text-3xl font-bold mb-5 text-blue-500">Our Story</h2>
-          <p>{aboutUsData.ourStory.text}</p>
-        </div>
-      </div>
+    <div className="flex flex-col items-center mx-20  ">
+      <div className="flex flex-col  lg:flex-row mt-8 lg:gap-10 lg:items-center justify-end items-center  mx-auto w-full lg:max-w-screen-xl">
+    <div className="relative lg:ml-0 ml-44 mt-5">
+        <Image src={dots} width={400} alt='dots' className='absolute top-10 -left-20 -z-10 rounded w-20' />
+        <Image src={translationRelative} width={400} height={350} alt='transltion-relative' className='rounded-md ' />
+        <Image src={tranlationPerson} width={280} alt='transltion-relative' className='absolute -left-36 bottom-0 rounded-md' />
+    </div>
+    <div className="lg:w-1/2  mt-4 md:mt-0 leading-relaxed p-2">
+    <h1 className="text-2xl font-bold mb-5 text-left text-blue-400">
+      Our Story
+      </h1>
+        <p className="text-sm tracking-wide leading-6">{aboutUsData.ourStory.text}</p>
+    </div>
+</div>
+</div>
 
-      <div className="flex flex-col md:flex-row mt-8">
-        <div className="md:w-1/2">
-          <img
-            src={aboutUsData.ourMission.image}
-            alt="Our Mission Image"
-            className="w-full"
-          />
-        </div>
-        <div className="md:w-1/2 mt-4 md:mt-0">
-          <p>{aboutUsData.ourMission.text}</p>
-        </div>
-      </div>
-
-      <div className="flex flex-col md:flex-row mt-8">
-        <div className="md:w-1/2">
-          <img
-            src={aboutUsData.whatSetsUsApart.image}
-            alt="What Sets Us Apart Image"
-            className="w-full"
-          />
-        </div>
-        <div className="md:w-1/2 mt-4 md:mt-0">
-          <p>{aboutUsData.whatSetsUsApart.textOne}</p>
-          <p>{aboutUsData.whatSetsUsApart.textTwo}</p>
-          <br />
-        </div>
-      </div>
-
+<div className="flex flex-col items-center mx-20 ">
+      <div className="flex flex-col lg:flex-row mt-8 lg:gap-x-48 justify-center items-center mx-auto max-w-screen-xl">
+      <div className="lg:w-1/2 mt-4 md:mt-0 leading-relaxed p-2">
+      <h1 className="text-2xl font-bold mb-5 text-left text-blue-400">
+      Our Mission
+      </h1>
+        <p className="text-sm tracking-wide leading-6">{aboutUsData.ourMission.text}</p>
+    </div>
+    <div className="relative lg:ml-0 ml-36 mt-5">
+        <Image src={dots} width={400} alt='dots' className='absolute top-10 -left-20 -z-10 rounded w-20' />
+        <Image src={translationRelative} width={400} height={350} alt='transltion-relative' className='rounded-md ' />
+        <Image src={tranlationPerson} width={280} alt='transltion-relative' className='absolute -left-36 bottom-0 rounded-md' />
+    </div>
+</div>
+</div>
+ 
+<div className="flex flex-col items-center mx-20  ">
+      <div className="flex flex-col  lg:flex-row mt-8 lg:gap-10 lg:items-center justify-end items-center  mx-auto w-full lg:max-w-screen-xl">
+    <div className="relative lg:ml-0 ml-44 mt-5">
+        <Image src={dots} width={400} alt='dots' className='absolute top-10 -left-20 -z-10 rounded w-20' />
+        <Image src={translationRelative} width={400} height={350} alt='transltion-relative' className='rounded-md ' />
+        <Image src={tranlationPerson} width={280} alt='transltion-relative' className='absolute -left-36 bottom-0 rounded-md' />
+    </div>
+    <div className="lg:w-1/2  mt-4 md:mt-0 leading-relaxed p-2">
+    <h1 className="text-2xl font-bold mb-5 text-left text-blue-400">
+    What Sets Us Apart
+      </h1>
+      <p className="text-sm tracking-wide leading-6">{aboutUsData.whatSetsUsApart.textOne}</p>
+      <p className="text-sm mt-5 tracking-wide leading-6">{aboutUsData.whatSetsUsApart.textTwo}</p>
+    </div>
+</div>
+</div>
       {/* Add more story blocks here */}
     </div>
   );
